3317f46deabf736ae930b2f2938a0bb75dbcdda1
Pedro Manjarres <122238970+amopanta@users.noreply.github.com>
2026-06-12 21:52:00 -0500

    frontend: enrutar builder y runtime

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BuilderApp } from './modules/builder/BuilderApp';
 import { RuntimeApp } from './modules/runtime/RuntimeApp';
 import './styles.css';
 
+function AppRouter() {
+  const path = window.location.pathname;
+  if (path.startsWith('/builder')) {
+    return <BuilderApp />;
+  }
+  return <RuntimeApp />;
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RuntimeApp />
+    <AppRouter />
   </React.StrictMode>,
 );
